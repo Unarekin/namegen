d07155260dcedb9b867fab68d5f1e24d021a1e67
Erica Phelps <erica@blackspork.com>
2021-03-09 18:29:13 -0600

    Added ability to call from CLI

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,25 @@ export class NameGenerator {
 
     // Load known templates
     this._templateHash = NameFiles;
+
+    this.GenerateName = this.GenerateName.bind(this);
+    this.GetTemplateName = this.GetTemplateName.bind(this);
+    this.GetTemplates = this.GetTemplates.bind(this);
+  }
+
+  public GetTemplateName(template: string): string {
+    if (!this._templateHash[template.toLowerCase()])
+      throw new Error(`Unknown template: '${template}'`);
+    return this._templateHash[template.toLowerCase()].name;
+  }
+
+  public GetTemplates(): { name: string, subsets: string[] }[] {
+    return Object.values(this._templateHash).map((val) => {
+      return {
+        name: val.name,
+        subsets: Object.keys(val.subsets)
+      }
+    }) as any;
   }
 
   public GenerateName(template: string, subset: string): string
@@ -39,17 +58,17 @@ export class NameGenerator {
     // Error checking
     if (typeof (firstArg) === "string" && typeof (secondArg) === "string") {
       if (!this._templateHash[firstArg.toLowerCase()])
-        throw new Error(`Unknown template: ${firstArg}.`);
+        throw new Error(`Unknown template: '${firstArg}'.`);
       if (!this._templateHash[firstArg.toLowerCase()].subsets[secondArg.toLowerCase()])
-        throw new Error(`Unknown subset: ${secondArg}`);
+        throw new Error(`Unknown subset: '${secondArg}'`);
       if (!(<any>this._templateHash[firstArg.toLowerCase()].subsets[secondArg.toLowerCase()]).originators)
-        throw new Error(`Unknown subset: ${secondArg}`);
+        throw new Error(`Unknown subset: '${secondArg}'`);
       actualSubset = <any>this._templateHash[firstArg.toLowerCase()].subsets[secondArg.toLowerCase()];
     } else if (firstArg.originators) {
       actualSubset = firstArg;
     } else if (firstArg.subsets) {
       if (!firstArg.subsets[secondArg.toLowerCase()])
-        throw new Error(`Unknown subset: ${secondArg}`);
+        throw new Error(`Unknown subset: '${secondArg}'`);
       actualSubset = firstArg.subsets[secondArg.toLowerCase()];
     } else if (Array.isArray(firstArg) && typeof (firstArg[0]) === "string") {
       actualSubset = this._parser.ParseJSON({ name: "temp", subsets: { "temp": firstArg } }).subsets.temp;
@@ -78,3 +97,52 @@ export class NameGenerator {
 /** Alias export, for backwards compatability. */
 /** Even if it's super unlikely anyone's actually using this. */
 export { NameGenerator as namegen };
+
+
+
+/**
+ * If it's called from the command line.
+ */
+if (require.main === module) {
+  (async function () {
+    try {
+      let generator = new NameGenerator();
+      if (process.argv[2] == "templates") {
+
+        console.log(`Template:           Subsets`);
+        console.log("=".repeat(50));
+
+        generator.GetTemplates().forEach((template) => {
+          console.log(`${template.name}: ${" ".repeat(20 - template.name.length)}${template.subsets.join("\t")}`);
+        });
+
+        return;
+      }
+
+      if (process.argv.length < 3)
+        throw new Error(`Usage: ${path.basename(process.argv[1])} <template> <subset> [<qty>]`);
+
+      let template: string = process.argv[2];
+      let subset: string = process.argv[3];
+      let qty: number = parseInt(process.argv[4] ?? "1");
+
+      if (isNaN(qty))
+        throw new Error(`'${process.argv[4]}' does not appear to be a number.`);
+
+      let names: string[] = [];
+
+      let templateName = generator.GetTemplateName(template);
+
+      for (let i = 0; i < qty; i++)
+        names.push(generator.GenerateName(template, subset));
+
+      let header: string = `${qty} ${templateName} name${qty > 1 ? 's' : ''}:`;
+      console.log(header);
+      console.log("=".repeat(header.length));
+      console.log(names.join("\n"));
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  })();
+}
